b56457b5f62baa6b0a06b2c59e062c804837309c
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-19 00:46:45 +0500

    create code 3

---
+++ HomeWork_3_seminar_2/Program.cs
@@ -0,0 +1,40 @@
+﻿System.Console.Write("--> ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch(a)
+{
+    case 1: 
+    System.Console.WriteLine("Не является выходным днём");
+    break;
+
+    case 2: 
+    System.Console.WriteLine("Не является выходным днём");
+    break;
+
+    case 3: 
+    System.Console.WriteLine("Не является выходным днём");
+    break;
+
+    case 4: 
+    System.Console.WriteLine("Не является выходным днём");
+    break;
+
+    case 5: 
+    System.Console.WriteLine("Не является выходным днём");
+    break;
+
+    case 6: 
+    System.Console.WriteLine("Суббота - ВЫХОДНОЙ ДЕНЬ");
+    break;
+
+    case 7: 
+    System.Console.WriteLine("Воскресенье - ВЫХОДНОЙ ДЕНЬ");
+    break;
+
+    default:
+        Console.WriteLine("Нет такого дня недели");
+        break;
+
+
+}
